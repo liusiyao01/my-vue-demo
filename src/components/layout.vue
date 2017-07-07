deb84722e719e61f9d91d20b07be906a34447169
liusiyao01 <1964098612@qq.com>
2017-07-07 16:23:16 +0800

    header footer

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    <div class="app-header">header</div>
-
-    <div class="app-content">content</div>
-    <div class="app-footer">footer</div>
+    <div class="app-head">
+      <div class="app-head-inner">
+        <img src="../assets/logo.png"/>
+        <div class="head-nav">
+          <ul class="nav-list">
+            <li>登录</li>
+            <li class="nav-pile">|</li>
+            <li>注册</li>
+            <li class="nav-pile">|</li>
+            <li>关于</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="app-content">
+      <router-view></router-view>
+    </div>
+    <div class="app-foot">
+      <p>© 2016 fishenal MIT</p>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -4,16 +4,27 @@ import Vue from 'vue';
 import Layout from './components/layout';
 import VRouter from 'vue-router';
 import Vuex from 'vuex';
-// import Apple from './components/apple';
-// import Banana from './components/banana';
-// import RedApple from './components/redApple';
+
+// components
+import IndexPage from './page/index';
 
 Vue.use(VRouter);
 Vue.use(Vuex);
 
+let router = new VRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: IndexPage
+    }
+  ]
+});
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   template: '<Layout/>',
   components: { Layout }
 });
